a9e05bfa0dec5901a2106c908d138640ef7a2f62
mipo47 <mipo3815@gmail.com>
2018-11-30 11:38:34 +0200

    firebase usage example

--- src/app/logger.ts
+++ src/app/logger.ts
@@ -1,11 +1,39 @@
 import { reaction } from 'mobx'
+import firebase from 'react-native-firebase'
 import TequilApiState from '../libraries/tequil-api/tequil-api-state'
 import VpnAppState from './vpn-app-state'
+import {Platform} from "react-native";
+
+const anal = firebase.analytics()
+// const crash = firebase.crashlytics()
 
 export default class Logger {
   private loggingStarted: boolean = false
+  private originalInfo: (...args: Object[]) => void
+  // private originalWarn: (...args: Object[]) => void
 
   constructor (private readonly tequilApiState: TequilApiState, private readonly vpnAppState: VpnAppState) {
+    //TODO: this is temporary for testing purpose
+    this.originalInfo = console.info
+    console.info = async (...args: Object[]) => {
+      this.originalInfo(...args)
+      try {
+        await anal.logEvent("console_info", { a: 123, platform: Platform.OS })
+      } catch (e) {
+        console.log('error while logEvent', e)
+      }
+    }
+
+    //TODO: remove or enable crashlitics
+    // this.originalWarn = console.warn
+    // console.warn = async (...args: Object[]) => {
+    //   this.originalWarn(...args)
+    //   try {
+    //     await crash.recordError(0, args[0].toString())
+    //   } catch (e) {
+    //     console.log('error while logEvent', e)
+    //   }
+    // }
   }
 
   public logObservableChanges (): void {
